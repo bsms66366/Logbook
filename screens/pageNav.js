308beb29ddc2cf05e551b6b0c060a93b3c283081
bsms66366 <bsms66366@users.noreply.github.com>
2020-03-10 13:13:05 +0000

    rebuilt pag nav
    still got errors

--- screens/pageNav.js
+++ screens/pageNav.js
@@ -1,5 +1,3 @@
-//This is an example code for ViewPager//
-import React, { Component } from 'react';
 //import react in our code.
 import { StyleSheet, View, Text, Image, Button } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
@@ -14,6 +12,7 @@ import {
   PagerDotIndicator,
 } from 'rn-viewpager';
 //import PagerTabIndicator , IndicatorViewPager , PagerTitleIndicator , PagerDotIndicator
+
 /**
    * Log out an example event after zooming
    *
@@ -35,12 +34,10 @@ export default class App extends Component {
   render() {
     return (
       <View style={{ flex: 1, marginTop: 1 }}>
-      
 
-
-        {/*Example of Dot Indicator*/}
+      {/*Example of Dot Indicator*/}
         <IndicatorViewPager
-          style={{ height: 450 }}
+          style={{ height: 200 }}
           indicator={this._renderDotIndicator()}>
           {/*_renderDotIndicator() will return the PagerDotIndicator*/}
           <View
@@ -62,42 +59,18 @@ export default class App extends Component {
             bindToBorders={true}
             onZoomAfter={this.logOutZoomState}
             style={styles.zoomableView}/>
-          
-
-            <Text style={{ color: 'white', fontSize: 20, marginBottom: 30 }}>THE BSMS LOGBOOK</Text>
+          <Text style={{ color: 'white', fontSize: 20, marginBottom: 30 }}>THE BSMS LOGBOOK</Text>
              <Image source={require('../assets/images/ClinicalSkillsLogo3.png')} style={{width: 100, height: 130, }} />
              <Text style={{ color: 'white', fontSize: 15 }}>Swipe to login</Text> 
-          </View>
-          <View
-            style={{
-              backgroundColor: '#FF5733',
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text style={{ color: 'white', fontSize: 30 }}>Introduction</Text>
-            <Text style={{ color: 'white', fontSize: 10, marginLeft: 20 }}>Welcome to the clinical practice component of Phase 1 teaching. The aim of your placements in both community and secondary care is to facilitate the development of your communication and examination skills in a protected environment. 
-You will be able to observe your clinical teachers seeing patients and, as you become more proficient, you will be allowed to practise some clinical skills under close supervision. This log book contains details of your afternoon rotations, dress code, and some guidelines e.g. confidentiality. 
-  This logbook should also be used to record your attendance at clinical sessions, and will be checked at the end of the year for your attendance profile, so please look after it. I would recommend that you take a photograph or copy of the attendance page at regular intervals. If you lose your logbook, there is evidence of your attendance prior to the event. If you do lose it, please contact the Clinical Practice School Office immediately (WatsonSchoolOffice@bsms.ac.ukor telephone: 01273 644768)</Text>
-          </View>
-          <View
-            style={{
-              backgroundColor: '#026666',
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text style={{ color: 'white', fontSize: 20 }}>Press here to login</Text>
-            <Text style={{ fontSize: 10, color: '#fff', marginLeft: 20 }}>The acquisition of clinical skills in BSMS will be progressive in both range and depth, in keeping with the overall shape of the curriculum and with the growing academic, professional and personal maturity of the student. The medical school endorses that the maintenance of safe and effective clinical skills requires continuing repetition, reflection, and refinement. </Text>
+You will be able to observe your clinical teachers seeing patients and, as you b
          
             <Button title="Login" onPress={() => {WebBrowser.openBrowserAsync('https://www.nhseportfolios.org/Anon/Login/Login.aspx'),alert('Now login to your Eportfolio!');}}/>
-            
             </View></View>
           </ReactNativeZoomableView>    
         </IndicatorViewPager>
+      </View>
         </View>
           </View>
-            </View>
         
           
           
@@ -112,21 +85,7 @@ You will be able to observe your clinical teachers seeing patients and, as you b
   _renderTitleIndicator() {
     return <PagerTitleIndicator titles={['One', 'Two', 'Three']} />;
   }
-
-  _renderDotIndicator() {
-    return <PagerDotIndicator pageCount={3} />;
-  }
-  _renderTabIndicator() {
-    let tabs = [
-      {
-        text: 'One',
-      },
-      {
-        text: 'Two',
-      },
-      {
-        text: 'Three',
-      },
+You will be able to observe your clinical teachers seeing patients and, as you b
     ];
     return <PagerTabIndicator tabs={tabs} />;
   }
